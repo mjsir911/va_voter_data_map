d2c1b1609c1a526c07c5d6b516227d7bdfc88009
Marco J Sirabella <marco@sirabella.org>
2017-07-10 16:16:37 -0400

    - Made multiscraper scrape again

--- src/multiscraper.py
+++ src/multiscraper.py
@@ -31,16 +31,16 @@ valid_years = [int(year.get_attribute('innerHTML')) for year in
         '#SearchYearFrom'
     ).find_elements_by_css_selector('option')
 ]
+
+
 def process_one_year(which_year):
     driver = init()
     for year in years(driver, which_year):
-        print(year)
         for i in pages(driver):
             for row in driver.find_elements_by_css_selector(
                     'tr.election_item'
             ):
-                pass
-                # getCSV(row)
+                getCSV(row)
 
 
 from multiprocessing import Pool
